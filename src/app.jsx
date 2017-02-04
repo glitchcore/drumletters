b37d6adbbbacdb849757622788da28963abc3ca7
Anper <mail@s3f.ru>
2017-02-04 16:20:52 +0300

    basic layout

--- src/app.jsx
+++ src/app.jsx
@@ -7,12 +7,31 @@ import { connect } from 'react-redux';
 import * as actions from './actions';
 import * as actionsType from "./actions_type";
 import configureStore from './store';
+import * as BS from 'react-bootstrap';
 
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        ПРИВЕТ
+      <div className="drumletters-wrapper">
+        <div className="drumletters-title">
+          Benny Greb letters for drummers
+        </div>
+        <BS.Row className="drumletters-main">
+          <BS.Col md={10} className="drumletters-letters">
+            Буквы
+          </BS.Col>
+          <BS.Col md={2} className="drumletters-selectors">
+            Чекбоксы
+          </BS.Col>
+        </BS.Row>
+        <BS.Row className="drumletters-footer">
+          <BS.Col md={7} className="drumletters-metro">
+            Метроном
+          </BS.Col>
+          <BS.Col md={5} className="drumletters-sequencer">
+            Секвенсер
+          </BS.Col>
+        </BS.Row>
       </div>
     )
   }
